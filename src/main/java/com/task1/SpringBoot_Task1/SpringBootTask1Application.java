76f80b793834890fb613b727d450c75abb5d6c9f
kot0fey <prutsk@mail.ru>
2023-04-26 11:28:47 +0300

    Initial commit

---
+++ src/main/java/com/task1/SpringBoot_Task1/SpringBootTask1Application.java
@@ -0,0 +1,13 @@
+package com.task1.SpringBoot_Task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/task1/SpringBoot_Task1/SpringBootTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task1.SpringBoot_Task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
